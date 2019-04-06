2fb9743bc5d756f324b513677171b86d18af143d
kashsid <kashifsiddique@gmail.com>
2019-04-05 21:51:07 -0500

    Implements routes in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact Path="/" component={Feeling} />
+          <Route exact Path="/understanding" component={Understanding} />
+          <Route exact Path="/Support" component={Supported} />
+          <Route exact Path="/comments" component={Comments} />
+          <Route exact Path="/review" component={FeedbackReview} />
+          <Route exact Path="admin" component={Admin} />
+        </div>
+      </Router>
     );
   }
 }
